227e861e5de56990bc254e83611cf6f86150e717
Maria Luisa Acosta Sanjuan <maluacsa@hotmail.com>
2021-08-20 10:23:34 -0500

    Modificación a los EndPoints para que coincidan con CRUD en la tala usuarios

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,20 +1,97 @@
 const {request, response} = require("express");
+const pool = require("../db/conexion");
+const usuariosQueries = require("../db/models/usuarios");
 
-const usuariosGet = (req = request, res = response) =>{
-    res.json({ msg: "Hola a todos desde GET"});
+const usuariosGet = async(req = request, res = response) =>{
+   let conn;
+
+
+   try {
+    conn = await pool.getConnection();
+
+    const usuarios = await conn.query(usuariosQueries.selectUsuarios);
+
+    res.json({usuarios});
+} catch (error) {
+        console.log(error);
+        res
+        .status(500)
+        .json({msg: "Por favor consulte  al admor.", error});
+} finally {
+        if (conn) conn.end();
+    }
+};
+const usuariosPost = async(req = request, res = response) =>{
+    const {nombre, email, password, status=1 } = req.body;
+    let conn;
+
+
+    try {
+     conn = await pool.getConnection();
+ 
+     const usuarios = await conn.query(usuariosQueries.insertUsuario,[
+         nombre, 
+         email, 
+         password, 
+         status]);
+ 
+
+         res.json({usuarios});
+ } catch (error) {
+         console.log(error);
+         res
+         .status(500)
+         .json({msg: "Por favor consulte  al admor.", error});
+ } finally {
+         if (conn) conn.end();
+     }
 }
 
-const usuariosPost = (req = request, res = response) =>{
-    const {nombre, apellido, email, edad=0 } = req.body;
-    res.status(201).json({ msg: "Hola a todos desde POST",edad});
+
+const usuariosPut = async (req = request, res = response) =>{
+    const { email } = req.query;
+    const { nombre, status } = req.body;
+    let conn;
+
+
+    try {
+     conn = await pool.getConnection();
+ 
+     const usuarios = await conn.query(usuariosQueries.updateUsuario, [
+         nombre, 
+         status,
+         email,
+        ]);
+ 
+     res.json({usuarios});
+ } catch (error) {
+         console.log(error);
+         res
+         .status(500)
+         .json({msg: "Por favor consulte  al admin.", error});
+ } finally {
+         if (conn) conn.end();
+     }
 }
-const usuariosPut = (req = request, res = response) =>{
-    const {id} = req.params;
-    res.status(400).json({ msg: "Hola a todos desde PUT",id}); 
-}
-const usuariosDelete = (req = request, res = response) =>{
-    const {usuario, password } = req.query;
-    res.status(500).json({ msg: "Hola a todos desde DELETE",usuario,password});   
+const usuariosDelete = async(req = request, res = response) =>{
+    const {email} = req.query;
+    let conn;
+
+    try {
+     conn = await pool.getConnection();
+ 
+     const usuarios = await conn.query(usuariosQueries.deleteUsuario, [email]);
+ 
+     res.json({usuarios});
+ } catch (error) {
+         console.log(error);
+         res
+         .status(500)
+         .json({msg: "Por favor consulte  al admin.", error});
+ } finally {
+         if (conn) conn.end();
+     }
+
 }
 
 module.exports = {usuariosGet,usuariosPost, usuariosPut, usuariosDelete};
